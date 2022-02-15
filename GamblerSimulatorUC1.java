c548d62d95a9480816595a4a5045ae5f7382160e
priyanka <priyanka.chavan1408@gmail.com>
2022-02-15 11:21:26 +0530

    ADDED: Gambler would start with a stake of  every day and bet  every game.

---
+++ GamblerSimulatorUC1.java
@@ -0,0 +1,11 @@
+package com.gambling.simulator;
+
+public class GamblerSimulatorUC1 {
+    public static final int AMOUNT_OF_STAKE_PER_DAY = 100 ;
+    public static final int AMOUNT_OF_BET_PER_GAME = 1;
+
+    public static void main(String[] args) {
+        System.out.println("Amount of stake per day is " + AMOUNT_OF_STAKE_PER_DAY+"$");
+        System.out.println("Amount of bet per game is " + AMOUNT_OF_BET_PER_GAME+"$");
+    }
+}

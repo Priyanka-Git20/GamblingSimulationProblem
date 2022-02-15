bd38ee78efe37cd5e8ce9ac5f45e4d85aac15921
priyanka <priyanka.chavan1408@gmail.com>
2022-02-15 11:15:36 +0530

    ADDED:Welcome message on the master branch.

---
+++ GamblerSimulator.java
@@ -0,0 +1,7 @@
+package com.gambling.simulator;
+
+public class GamblerSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Gambling simulation program.");
+    }
+}
